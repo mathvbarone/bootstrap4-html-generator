436eef598c5cf20730f22d8be0f50b1cd96814a1
Matheus Barone <matheus.barone@avanade.com>
2018-04-30 10:07:30 -0300

    Improving JS code

--- script.js
+++ script.js
@@ -1,61 +1,53 @@
-const inputNumber = document.querySelector('.columns-number-input');
-const generateButton = document.querySelector('.generate-button');
-const cleanButton = document.querySelector('.clean-button');
-const example = document.querySelector('.example');
-const showCode = document.querySelector('.show-code');
-let col;
-let columns;
 
-
-cleanCode = e => {
-    e.preventDefault();
-    showCode.innerHTML = '';
-    inputNumber.value = '';
-    example.innerHTML = '';
-}
-
-
-
-
-showExample = () => {
-    col = 
-`       <div class="col-example col-${ 12 / inputNumber.value }">
-        </div>
-`
-    columns = new Array(parseInt(inputNumber.value)).fill(col).join('');
-    example.innerHTML = columns;
-}
+(() => {
+    const inputNumber = document.querySelector('.columns-number-input');
+    const generateButton = document.querySelector('.generate-button');
+    const cleanButton = document.querySelector('.clean-button');
+    const example = document.querySelector('.example');
+    const showCode = document.querySelector('.show-code');
+    let col;
+    let columns;
     
- 
-generateCode = e => {
-e.preventDefault();
-            
-if(inputNumber.value <= 12 ){
-    showExample();
-        col = 
-    `   <div class="col-${ 12 / inputNumber.value }">
-        </div>
-    `
-    const openRow = [
-    `    <div class="row">
-    `];
-    columns = new Array(parseInt(inputNumber.value)).fill(col);
-    const closeRow = [
-    `</div>
-    `];
-    const code = openRow.concat(columns, closeRow).join('');
-
-    showCode.innerHTML = code;
-    inputNumber.value = '';
-    } else {
-        alert('Max 12 columns')
+    cleanCode = e => {
+        e.preventDefault();
+        showCode.innerHTML = '';
+        inputNumber.value = '';
+        example.innerHTML = '';
     }
-}
-
     
-
-
-
-
-generateButton.addEventListener('click', generateCode);
-cleanButton.addEventListener('click', cleanCode);
+    showExample = () => {
+        col = 
+    `       <div class="col-example col-${ 12 / inputNumber.value }">
+            </div>
+    `
+        columns = new Array(parseInt(inputNumber.value)).fill(col).join('');
+        example.innerHTML = columns;
+    }
+        
+    generateCode = e => {
+        e.preventDefault();
+        if(inputNumber.value >= 12 ){
+            alert('Max 12 columns')
+        } else {
+            showExample();
+                col = 
+            `   <div class="col-${ 12 / inputNumber.value }">
+                </div>
+            `
+            const openRow = [
+            `    <div class="row">
+            `];
+            columns = new Array(parseInt(inputNumber.value)).fill(col);
+            const closeRow = [
+            `</div>
+            `];
+            const code = openRow.concat(columns, closeRow).join('');
+    
+            showCode.innerHTML = code;
+            inputNumber.value = '';
+        }
+    }
+    
+    generateButton.addEventListener('click', generateCode);
+    cleanButton.addEventListener('click', cleanCode);
+});
