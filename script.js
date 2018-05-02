b21f24094d10d44ccdbc7a727f471f34aaf28adc
Matheus Barone <matheus.barone@avanade.com>
2018-05-02 09:40:46 -0300

    Implementing space option

--- script.js
+++ script.js
@@ -5,30 +5,39 @@
     const cleanButton = document.querySelector('.clean-button');
     const example = document.querySelector('.example');
     const showCode = document.querySelector('.show-code');
+    const columnsSpace = document.querySelector('.columns-space');
+    const columnsSpaceSet = document.querySelector('.columns-space-set');
     let col;
     let columns;
     
     cleanCode = e => {
         e.preventDefault();
         showCode.innerHTML = '';
-        inputNumber.value = '';
+        inputNumber.value = 'number-of-columns';
+        columnsSpace.value = 'number-of-columns'
         example.innerHTML = '';
+        
     }
     
     showExample = () => {
-        col = 
-    `       <div class="col-example col-${ 12 / inputNumber.value }">
-            </div>
-    `
         columns = new Array(parseInt(inputNumber.value)).fill(col).join('');
         example.innerHTML = columns;
     }
-        
+    
+    checkSpace = () => {
+        if(columnsSpace.value === 'space-columns-yes') {
+            columnsSpaceSet.classList.add('is-active');
+        } else {
+            columnsSpaceSet.classList.remove('is-active');
+        }
+    }
+
     generateCode = e => {
         e.preventDefault();
-        if(inputNumber.value >= 12 ){
+        if (inputNumber.value > 12 ){
             alert('Max 12 columns')
         } else {
+            console.log(inputNumber.value % 2);
             showExample();
                 col = 
             `   <div class="col-${ 12 / inputNumber.value }">
@@ -44,10 +53,10 @@
             const code = openRow.concat(columns, closeRow).join('');
     
             showCode.innerHTML = code;
-            inputNumber.value = '';
         }
     }
     
+    columnsSpace.addEventListener('click', checkSpace);
     generateButton.addEventListener('click', generateCode);
     cleanButton.addEventListener('click', cleanCode);
-});
+})();
